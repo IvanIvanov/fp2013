8031432a2e5c474d71f6374127c7c3c253f47930
Hristo Vladev <hr.vladev@gmail.com>
2013-10-14 02:20:03 +0300

    Add tests for the second homework of group 4.

---
+++ lab4/10-10-2013/homework/solution_test.scm
@@ -0,0 +1,36 @@
+(load "cassie.scm")
+(load "")
+
+(define-test balanced?-test
+  (assert (balanced? 0)       #t)
+  (assert (balanced? 9)       #t)
+  (assert (balanced? 11)      #t)
+  (assert (balanced? 13)      #f)
+  (assert (balanced? 121)     #t)
+  (assert (balanced? 4518)    #t)
+  (assert (balanced? 9122)    #f)
+  (assert (balanced? 28471)   #f)
+  (assert (balanced? 1238033) #t))
+
+(define-test even-fibonacci-sum-test
+  (assert (even-fibonacci-sum 0)  0)
+  (assert (even-fibonacci-sum 1)  0)
+  (assert (even-fibonacci-sum 2)  0)
+  (assert (even-fibonacci-sum 3)  0)
+  (assert (even-fibonacci-sum 4)  2)
+  (assert (even-fibonacci-sum 10) 44)
+  (assert (even-fibonacci-sum 42) 82790070)
+  (assert (even-fibonacci-sum 42) 286573922006908542050))
+
+(define-test sum-of-prime-divisors-test
+  (assert (sum-of-prime-divisors 4)  2)
+  (assert (sum-of-prime-divisors 10) 7)
+  (assert (sum-of-prime-divisors 36) 5)
+  (assert (sum-of-prime-divisors 42) 12)
+  (assert (sum-of-prime-divisors 17) 17)
+  (assert (sum-of-prime-divisors 1878) 318))
+
+(run-tests
+  balanced?-test
+  even-fibonacci-sum-test
+  sum-of-prime-divisors-test)
